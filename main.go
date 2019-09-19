a81b7522d6c9513fc0af88dea964479e2171d5c5
yihong <yihongliu00@gmail.com>
2019-09-19 12:29:54 -0400

    testing https

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	//"fmt"
 	"log"
 	"net/http"
+	"crypto/tls"
+	"golang.org/x/crypto/acme/autocert"
 
 	"github.com/gorilla/mux"
 	app "github.com/lyihongl/main/snippet/controllers"
@@ -12,6 +14,12 @@ import (
 )
 
 func main() {
+
+	certManager := autocert.Manager{
+		Prompt:		autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("yihong.ca"),
+		Cache:		autocert.DirCache("certs"),
+	}
 
 	//go globalSessions.GC
 	data.Init()
@@ -31,7 +39,19 @@ func main() {
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("snippet/views/static"))))
 	r.PathPrefix("/dynamic/").Handler(http.StripPrefix("/dynamic/", http.FileServer(http.Dir("snippet/javascript"))))
 
-	err := http.ListenAndServe(":9090", r) //set listen port
+	server := &http.Server{
+		Addr: ":https",
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
+		Handler: r,
+	}
+
+	go http.ListenAndServe(":http", certManager.HTTPHandler(nil))
+
+	log.Fatal(server.ListenAndServeTLS("", ""))
+
+	err := http.ListenAndServe(":443", r) //set listen port
 	if err != nil {
 		log.Fatal("ListenAndServer:", err)
 	}
