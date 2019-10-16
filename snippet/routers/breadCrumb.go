c581079d89f9ac79e35c6b512bc8655dde3c9e9e
yihong <yihongliu00@gmail.com>
2019-10-15 22:53:50 -0400

    created breadcrumb level system

--- snippet/routers/breadCrumb.go
+++ snippet/routers/breadCrumb.go
@@ -1,19 +1,41 @@
 package controllers
 
-const(
-	ROOT = iota
+import (
+	//"net/http"
+
+	//"github.com/gorilla/mux"
+)
+
+const (
+	ROOT    = iota
 	LEVEL_1 = iota
 	LEVEL_2 = iota
 )
 
+
 type BreadCrumb struct {
-	Title, Path, Level string
+	Title, Path string
+	Level int
 }
 
 type BreadCrumbList struct {
 	Crumbs []BreadCrumb
+	Levels map[string]int
 }
 
 func (b *BreadCrumbList) Push(crumb *BreadCrumb) {
 	b.Crumbs = append(b.Crumbs, *crumb)
+}
+
+func (b *BreadCrumbList) DefineLevels(levels map[string]int) {
+	b.Levels = levels
+}
+
+func (b *BreadCrumbList) Update(action string) {
+	for i, e := range b.Crumbs{
+		if(e.Level <= b.Levels[action]) {
+			b.Crumbs[len(b.Crumbs) - 1], b.Crumbs[i] = b.Crumbs[i], b.Crumbs[len(b.Crumbs)-1]
+			b.Crumbs = b.Crumbs[:len(b.Crumbs) - 1]
+		}
+	}
 }
--- snippet/routers/generalRounter.go
+++ snippet/routers/generalRounter.go
@@ -13,6 +13,18 @@ func ServiceRouter(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	fmt.Println(vars)
 	fmt.Println(r.Method)
+
+	var snippetCrumbs BreadCrumbList
+
+	var levels = map[string]int{
+		"home":ROOT,
+		"edit":LEVEL_1,
+		"create":LEVEL_1,
+	}
+
+	snippetCrumbs.DefineLevels(levels)
+	snippetCrumbs.Update(vars["action"])
+
 	print("url action: " + vars["service"] + "\n")
 	if vars["service"] == "" {
 		app.ServicePage(w, r)
