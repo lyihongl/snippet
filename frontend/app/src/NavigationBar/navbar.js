a519b11d5b8f129212ece729d5aac7eb44546c65
yihong <yihongliu00@gmail.com>
2019-12-07 11:15:05 -0500

    saving work

--- frontend/app/src/NavigationBar/navbar.js
+++ frontend/app/src/NavigationBar/navbar.js
@@ -1,2 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./navbar.css"
+
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    componentDidMount() {
+
+    }
+    render() {
+        return (
+            <div>
+                <nav class="container main-nav">
+                    <div class="nav-item nav-1">
+                        <a href="/projects" class="navLinks">Projects</a>
+                    </div>
+                    <div class="nav-item nav-2">
+                        <a href="/" class="navLinks">Yihong Liu</a>
+                    </div>
+                    <div class="nav-item nav-3">
+                        <a href="/about" class="navLinks">About Me</a>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
--- frontend/app/src/index.js
+++ frontend/app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Navbar from './NavigationBar/navbar.js'
 //import App from './App';
 //import * as serviceWorker from './serviceWorker';
 
@@ -34,17 +35,21 @@ class App extends React.Component{
         super(props);
     }
     componentDidMount(){
-        
     }
     render(){
-
+        return (
+            <div>
+                <div class="navbar">
+                    <Navbar />
+                </div>
+            </div>
+        );
     }
 }
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
